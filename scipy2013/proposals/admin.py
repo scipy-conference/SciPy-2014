c08ed08cf5141fed81bd4761569f2f2d29ac920b
Jim Ivanoff <jim@polarbeardesign.net>
2013-11-14 14:02:32 -0600

    changing all 2013 to 2014

--- scipy2013/proposals/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from scipy2013.proposals.models import TalkPosterProposal, TutorialProposal, BofProposal, SprintProposal
-
-
-admin.site.register(TalkPosterProposal)
-admin.site.register(TutorialProposal)
-admin.site.register(BofProposal)
-admin.site.register(SprintProposal)
