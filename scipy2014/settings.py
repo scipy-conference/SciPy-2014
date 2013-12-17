b5f77d04a30ca39d6cd460dadfe24f624066c1cd
Jim Ivanoff <jim@polarbeardesign.net>
2013-12-17 12:13:08 -0600

    formatting for home page, adding img assets

--- scipy2014/settings.py
+++ scipy2014/settings.py
@@ -26,12 +26,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",  # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "dev.db",                        # Or path to database file if using sqlite3.
-        "USER": "",                              # Not used with sqlite3.
-        "PASSWORD": "",                          # Not used with sqlite3.
-        "HOST": "",                              # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                              # Set to empty string for default. Not used with sqlite3.
+        "ENGINE": "django.db.backends.mysql",  # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "scipy_db",                        # Or path to database file if using sqlite3.
+        "USER": "jri",                              # Not used with sqlite3.
+        "PASSWORD": "tensai14",                          # Not used with sqlite3.
+        "HOST": "127.0.0.1",                              # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "3306",                              # Set to empty string for default. Not used with sqlite3.
     }
 }
 
