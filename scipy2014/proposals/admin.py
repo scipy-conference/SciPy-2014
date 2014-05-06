e99a6daee344505afa273c49000ac2361910d99b
codersquid <github@codersquid.com>
2014-05-06 15:10:08 -0500

    add cancelled to admin interface

--- scipy2014/proposals/admin.py
+++ scipy2014/proposals/admin.py
@@ -5,29 +5,28 @@ from scipy2014.proposals.models import TalkPosterProposal, TutorialProposal, Bof
 
 class TalkPosterProposalAdmin(admin.ModelAdmin):
     model = TalkPosterProposal
-    list_display = ['title', 'speaker', 'topic_track', 'domain_symposium']
-    list_filter = ['submission_type', 'topic_track', 'domain_symposium']
-    """
-    actions = [export_as_csv_action("CSV Export", fields=[
-        "id",
-        "title",
-        "speaker",
-        "speaker_email",
-        "kind",
-    ])]
-    """
+    list_display = [
+        'title',
+        'speaker',
+        'speaker_email',
+        'topic_track',
+        'domain_symposium',
+        'kind',
+        'cancelled',
+    ]
+    list_filter = ['submission_type', 'topic_track', 'domain_symposium', 'cancelled']
 
 admin.site.register(TalkPosterProposal, TalkPosterProposalAdmin)
 admin.site.register(TutorialProposal,
-        list_display = ['title', 'speaker', 'track'],
+        list_display = ['title', 'speaker', 'speaker_email', 'track', 'cancelled'],
         list_filter = ['track'],
         date_heirarchy='submitted',
         )
 admin.site.register(BofProposal, 
-        list_display = ['title', 'speaker',],
+        list_display = ['title', 'speaker', 'speaker_email', 'cancelled'],
         date_heirarchy='submitted',
         )
 admin.site.register(SprintProposal,
-        list_display = ['title', 'speaker',],
+        list_display = ['title', 'speaker', 'speaker_email', 'cancelled'],
         date_heirarchy='submitted',
         )
