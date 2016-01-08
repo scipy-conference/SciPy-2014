50f5be8b8aec06f5b7bda135eb2d781a471394e9
Juan B Cabral <jbc.develop@gmail.com>
2016-01-08 19:12:38 -0300

    added redirect to automatically go to scipyla app

--- scipyla2016/urls.py
+++ scipyla2016/urls.py
@@ -1,3 +1,5 @@
+
+from django.shortcuts import redirect
 from django.conf.urls.defaults import patterns, include
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +10,6 @@ WIKI_SLUG = r"(([\w-]{2,})(/[\w-]{2,})*)"
 
 urlpatterns = patterns(
     '',
+    (r'^$', lambda r: redirect("scipyla2016/", permanent=True)),
     (r'^scipyla2016/', include('scipyla2016._urls')),
 )
